0d3df0a342e06ca916ab348d1cd8b4e17d429ebb
Dipanjan1810 <dipanjandhar18@gmail.com>
2023-02-25 02:03:56 +0530

    Update ambulance.py

--- ambulance.py
+++ ambulance.py
@@ -18,4 +18,39 @@ class travel():
         self.root=root
         self.root.title("Ambulance Booking System")
         self.root.geometry(geometry)
+        
+        
+        DateofOrder=StringVar()
+        DateofOrder.set(time.strftime(" %d / %m / %Y "))
+        Receipt_Ref=StringVar()
+        TotalCost=StringVar()
+        
+        var1=IntVar()
+        var2=IntVar()
+        var3=IntVar()
+        var4=IntVar()
+        journeyType=IntVar()
+        
+        varl1=StringVar()
+        varl2=StringVar()
+        varl3=StringVar()
+        reset_counter=0
+        
+        Firstname=StringVar()
+        Surname=StringVar()
+        Address=StringVar()
+        Pincode=StringVar()
+        Mobile=StringVar()
+
+        Km=StringVar()
+        Receipt=StringVar()
                         
+        
+        def iExit():
+            iExit= ms.askyesno("Prompt!","Do you want to exit?")
+            if iExit > 0:
+                root.destroy()
+                return
+        
+        def Reset():
+            Km.set("")
